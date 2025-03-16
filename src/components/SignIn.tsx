390ab756b5bbff724306a578fc96017b5e10c0d8
George-25 <georgemallory26@gmail.com>
2025-03-16 16:23:58 -0400

    still working on sign in

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,25 +1,58 @@
 const SignIn = () => {
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 py-12 sm:px-6 lg:px-8">
-        <div id="heading" className="space-y-1">
+      <div className="flex flex-col max-w-lg min-h-60  bg-gray-100">
+        <div id="heading" className="space-y-2">
           <h2 className="text-2xl font-bold text-center">
             Sign in to your account
           </h2>
           <p className="text-center">
-            Enter your email and password to sign in
+            Enter your email and password to access your account
           </p>
         </div>
-        <form className="space-y-4">
-          <label htmlFor="email">Email</label>
-          <input type="email" placeholder="name@example.com" required />
-          <label htmlFor="password">Password</label>
-          <a href="#">Forgot password</a>
-          <input type="password" placeholder="........." required />
+        <form>
+          <div id="email" className="flex flex-col">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="name@example.com"
+              required
+              className="border-2 border-gray-700 focus:border-blue-600 rounded-sm "
+            />
+          </div>
+          <div id="password">
+            <div
+              id="link"
+              className="flex flex-row items-center justify-around"
+            >
+              <label htmlFor="password" className="font-semibold">
+                Password
+              </label>
 
-          <button type="submit">Sign In</button>
+              <a href="#" className="text-sm text-gray-500 hover:text-gray-900">
+                Forgot password?
+              </a>
+            </div>
+            <input
+              type="password"
+              placeholder="........."
+              required
+              className=" w-full border-2 border-gray-700 focus:border-blue-600 rounded-sm"
+            />
+          </div>
+
+          <button type="submit" className="w-full my-6 border">
+            Sign In
+          </button>
+          <div className="flex flex-row text-center text-sm text-gray-500 justify-center space-x-1">
+            <p>Don't have an account?</p>
+            <a href="#" className="text-gray-900 hover:underline">
+              Sign up
+            </a>
+          </div>
         </form>
-        <p>Don't have an account?</p>
       </div>
     </>
   );
