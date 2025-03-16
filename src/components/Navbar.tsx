2d8b1e84a6a2d49f20212f634fc942d2d7e62ad6
George-25 <georgemallory26@gmail.com>
2025-03-16 00:44:50 -0400

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,21 @@ import { FaHandsHelping } from "react-icons/fa";
 const Navbar = () => {
   return (
     <>
-      <nav id="nav" className="flex flex-row w-full justify-between px-2 py-2">
-        <div id="logo" className="flex">
+      <nav
+        id="nav"
+        className="flex flex-row w-full justify-between items-center  px-2 py-2 bg-sky-500/25"
+      >
+        <div id="logo" className="flex items-center space-x-1 text-xl">
           <FaHandsHelping />
-          <h1>Scriptee.me</h1>
+          <h1 className="text-zinc-500">Scriptee.me</h1>
         </div>
-        <ul className="flex space-x-4">
-          <li>SignUp</li>
-          <li>SignIn</li>
+        <ul className="flex space-x-4 ">
+          <li className="hover:text-blue-600">
+            <a href="#">SingUp</a>
+          </li>
+          <li className="hover:text-blue-600">
+            <a href="#">SignIn</a>
+          </li>
         </ul>
       </nav>
     </>
