8531c31c2ce87a80e4bc00e4142535469e84a3f5
George-25 <georgemallory26@gmail.com>
2025-03-16 11:46:13 -0400

    signup form done

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,6 +3,16 @@ const SignUp = () => {
     <>
       <div>
         <h2>Sign Up</h2>
+        <form>
+          <input type="email" placeholder="Email" />
+          <br />
+          <input type="password" placeholder="Password" />
+          <br />
+          <button type="submit">Sign Up</button>
+        </form>
+        <p>
+          Already have an account? <link>Sing In</link>
+        </p>
       </div>
     </>
   );
