ee251cdfc44a5faa644712960653a5820d58843c
George-25 <georgemallory26@gmail.com>
2025-03-16 12:06:23 -0400

    sign in file done

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,6 +3,16 @@ const SignIn = () => {
     <>
       <div>
         <h2>Sign In</h2>
+        <form>
+          <input type="email" placeholder="Email" />
+          <br />
+          <input type="password" placeholder="Password" />
+          <br />
+          <button type="submit">Sign In</button>
+        </form>
+        <p>
+          Don't have an account? <link>Sign In</link>
+        </p>
       </div>
     </>
   );
