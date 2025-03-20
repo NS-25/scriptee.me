46c3df0fa1c9818a9091912b45cf9ce4e42ae473
George-25 <georgemallory26@gmail.com>
2025-03-19 23:13:23 -0400

    added some codes

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,42 +1,42 @@
-
-import * as React, { useState, createContext } from "react";
+import { useState, createContext,useContext } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../components/firebase";
+import { auth } from "../services/firebase";
 
-const authContext = createContext(false);
-
+const authContext = createContext(null);// should be object
 
 function useAuth() {
   const [authed, setAuthed] = useState(false);
 
-    const login = (email, password) => {
-      signInWithEmailAndPassword(auth, email, password)
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        console.log("userCredential - signIn :", userCredential);
-        const user = userCredential.user;
-        localStorage.set('user', JSON.stringify(user))
+        console.log("User signed in: ", userCredential);
+
+        localStorage.setItem("user", JSON.stringify(userCredential.user)); // store user in local storage.
         setAuthed(true);
+        return userCredential.user;
       })
       .catch((error) => {
+        console.error("Login error: ", error.message);
         return error;
       });
-    }
+  };
 
-    const logout = () => {
-      console.log('logout')
-    }
+  const logout = () => {
+    console.log("logout");
+  };
 
-    return {
-      authed,
-      login,
-      logout
-    }
+  return (
+    authed,
+    login,
+    logout,
+);
 }
 
 export function AuthProvider({ children }) {
   const auth = useAuth();
 
-  return <authContext.Provider value={auth}>{children}</authContext.Provider>
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 export default function AuthConsumer() {
